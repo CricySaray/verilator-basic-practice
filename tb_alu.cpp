6bee7dd9ab1ee66b53c9875c65e163bdd09fd1f5
Song Anrui <circysaray@qq.com>
2024-01-09 16:10:52 +0800

    done the pt.1

--- tb_alu.cpp
+++ tb_alu.cpp
@@ -7,17 +7,20 @@
 #include "Valu.h"
 #include "Valu___024unit.h"
 
-#define MAX_SIM_TIME 20
+#define MAX_SIM_TIME 40
 vluint64_t sim_time = 0;
 
 int main(int argc, char** argv, char** env) {
-	Valu *dut = new Valu;
+	Valu *dut = new Valu; // instantiates converted ALU module
+												// In systemVerilog = `alu dut(.*);`
 
+	// set up the waveform dumping
 	Verilated::traceEverOn(true);
 	VerilatedVcdC *m_trace = new VerilatedVcdC;
-	dut->trace(m_trace, 5);
+	dut->trace(m_trace, 5); // pass m_trace on to `dut`
 	m_trace->open("waveform.vcd");
 
+	// make the simulation happen: (The Body of Simulation)
 	while (sim_time < MAX_SIM_TIME) {
 		dut->clk ^= 1; // here is ingenious.
 		dut->eval();
